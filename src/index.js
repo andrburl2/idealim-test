04a86b51507b47cb73861d3df7fe02b49bb4f14f
Andrey Burlakov <andrburl2@yandex.ru>
2022-03-05 14:04:16 +0300

    Создал секции Header и Slider, адаптировал их

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './styles/index.scss';
 import 'bootstrap';
+import $ from 'jquery';
+
+function showMobileNav() {
+  $('.nav__button').toggleClass('nav__button_active');
+  $('.nav__list').toggleClass('nav__list_hidden');
+}
+
+$('.nav__button').on('click',showMobileNav);
